4d740f98f2d86869e305061b51b499118ba006aa
DoctorPok42 <remi.mazat@epitech.eu>
2023-01-24 15:22:35 +0100

    mv config file

--- launch.sh
+++ launch.sh
@@ -7,8 +7,13 @@ sudo chmod -R 755 /var/www/intra.asrlab.lan
 sudo mkdir -p /var/www/intra-adm.asrlab.lan/html
 sudo chmod -R 755 /var/www/intra-adm.asrlab.lan
 
-sudo curl https://raw.githubusercontent.com/DoctorPok42/my_web/develop/nginx/intra.asrlab.lan >> sudo /etc/nginx/sites-available/intra.asrlab.lan
-sudo curl https://raw.githubusercontent.com/DoctorPok42/my_web/develop/nginx/intra-adm.asrlab.lan >> sudo /etc/nginx/sites-available/intra-adm.asrlab.lan
+sudo curl https://raw.githubusercontent.com/DoctorPok42/my_web/develop/nginx/intra.asrlab.lan >> intra.asrlab.lan
+sudo curl https://raw.githubusercontent.com/DoctorPok42/my_web/develop/nginx/intra-adm.asrlab.lan >> intra-adm.asrlab.lan
+
+sudo mv intra.asrlab.lan /etc/nginx/sites-available/intra.asrlab.lan
+sudo mv intra-adm.asrlab.lan /etc/nginx/sites-available/intra-adm.asrlab.lan
+
+sudo rm intra.asrlab.lan*
 
 sudo ln -s /etc/nginx/sites-available/intra.asrlab.lan /etc/nginx/sites-enabled/
 sudo ln -s /etc/nginx/sites-available/intra-adm.asrlab.lan /etc/nginx/sites-enabled/
