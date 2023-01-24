bb96a3de893b2d50755fd390c1c90ca9e7096204
DoctorPok42 <remi.mazat@epitech.eu>
2023-01-24 14:59:47 +0100

    fix errors

--- launch.sh
+++ launch.sh
@@ -9,39 +9,8 @@ sudo chmod -R 755 /var/www/intra-adm.asrlab.lan
 
 sudo nano /etc/nginx/sites-available/intra.asrlab.lan
 
-server {
-    listen 81;
-    listen [::]:81;
-
-    root /var/www/intra.asrlab.lan/html;
-    index index.html index.htm index.nginx-debian.html;
-
-    server_name intra.asrlab.lan www.intra.asrlab.lan;
-
-    location / {
-        try_files $uri $uri/ =404;
-    }
-}
-
-CTRL+X, Y, ENTER
-
-sudo nano /etc/nginx/sites-available/intra-adm.asrlab.lan
-
-server {
-    listen 82;
-    listen [::]:82;
-
-    root /var/www/intra-adm.asrlab.lan/html;
-    index index.html index.htm index.nginx-debian.html;
-
-    server_name intra-adm.asrlab.lan www.intra-adm.asrlab.lan;
-
-    location / {
-        try_files $uri $uri/ =404;
-    }
-}
-
-CTRL+X, Y, ENTER
+curl https://raw.githubusercontent.com/DoctorPok42/my_web/develop/nginx/intra.asrlab.lan >> /etc/nginx/sites-available/intra.asrlab.lan
+curl https://raw.githubusercontent.com/DoctorPok42/my_web/develop/nginx/intra-adm.asrlab.lan >> /etc/nginx/sites-available/intra-adm.asrlab.lan
 
 sudo ln -s /etc/nginx/sites-available/intra.asrlab.lan /etc/nginx/sites-enabled/
 sudo ln -s /etc/nginx/sites-available/intra-adm.asrlab.lan /etc/nginx/sites-enabled/
