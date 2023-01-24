1b5c257ed0a061564b3b68b09e993d82e1a95cda
DoctorPok42 <remi.mazat@epitech.eu>
2023-01-24 15:02:32 +0100

    update launch

--- launch.sh
+++ launch.sh
@@ -7,8 +7,6 @@ sudo chmod -R 755 /var/www/intra.asrlab.lan
 sudo mkdir -p /var/www/intra-adm.asrlab.lan/html
 sudo chmod -R 755 /var/www/intra-adm.asrlab.lan
 
-sudo nano /etc/nginx/sites-available/intra.asrlab.lan
-
 curl https://raw.githubusercontent.com/DoctorPok42/my_web/develop/nginx/intra.asrlab.lan >> /etc/nginx/sites-available/intra.asrlab.lan
 curl https://raw.githubusercontent.com/DoctorPok42/my_web/develop/nginx/intra-adm.asrlab.lan >> /etc/nginx/sites-available/intra-adm.asrlab.lan
 
